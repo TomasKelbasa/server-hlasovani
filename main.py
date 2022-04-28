882f7939d76e49d20ed06cd70be6c39320f13678
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-04-28 11:37:48 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 radio.set_group(15)
-odpovedi = {}
+odpovedi = [{"serial" : 156840, "vote": 3}]
+canVote = True
+
+
 radio.on_received_value(on_received_value)
 
 def on_received_value(name, value):
     global odpovedi
-    if name == "answer":
+    if name == "answer" and canVote:
+        zmeneno = False
         serial_num = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
-        
-def on_forever():
-    pass
-basic.forever(on_forever)
+        for o in odpovedi:
+            if o["serial"] == serial_num:
+                o["vote"] = value
+                zmeneno = True
+        if not zmeneno:
+            odpovedi.push({"serial" : serial_num, "vote": value})
+        radio.send_value("confirm", serial_num)
+
+def on_button_pressed_a():
+    for v in odpovedi:
+        print(String.from_char_code(v["vote"]+65))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    for i in range (0,5):
+        suma = 0
+        for p in odpovedi:
+            if p["vote"] == i:
+                suma += 1
+        print(String.from_char_code(i+65))
+        print(": ")
+        print(suma)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,16 +1,53 @@
 radio.setGroup(15)
-let odpovedi =  {
-	
+let odpovedi = [ {
+    "serial" : 156840,
+    "vote" : 3,
 }
-
+]
+let canVote = true
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let zmeneno: boolean;
     let serial_num: number;
     
-    if (name == "answer") {
+    if (name == "answer" && canVote) {
+        zmeneno = false
         serial_num = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        for (let o of odpovedi) {
+            if (o["serial"] == serial_num) {
+                o["vote"] = value
+                zmeneno = true
+            }
+            
+        }
+        if (!zmeneno) {
+            odpovedi.push( {
+                "serial" : serial_num,
+                "vote" : value,
+            }
+            )
+        }
+        
+        radio.sendValue("confirm", serial_num)
     }
     
 })
-basic.forever(function on_forever() {
-    
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    for (let v of odpovedi) {
+        console.log(String.fromCharCode(v["vote"] + 65))
+    }
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let suma: number;
+    for (let i = 0; i < 5; i++) {
+        suma = 0
+        for (let p of odpovedi) {
+            if (p["vote"] == i) {
+                suma += 1
+            }
+            
+        }
+        console.log(String.fromCharCode(i + 65))
+        console.log(": ")
+        console.log(suma)
+    }
 })
